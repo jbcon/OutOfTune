f40e7a11b22294f7ed58cfb40bfdbd133f8b6c29
John Conover <111jbcon@gmail.com>
2015-02-18 15:37:07 -0500

    Initial Unity Commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
