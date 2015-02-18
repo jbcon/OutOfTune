65cc558fd443b67ea2da6d8f76cb2aee68ecb5f6
John Conover <111jbcon@gmail.com>
2015-02-18 15:54:34 -0500

    Simple player movement, increased gravity for tighter jumps

--- OutOfTune/Assets/Scripts/Player/PlayerController.cs
+++ OutOfTune/Assets/Scripts/Player/PlayerController.cs
@@ -3,18 +3,42 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public float maxSpeed = 10f;
+    public float jumpForce = 10f;
+    bool facingRight = true;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-
-    void FixedUpdate()
-    {
-
-    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void FixedUpdate()
+    {
+        CharacterMovement();
+    }
+
+    void CharacterMovement()
+    {
+        LeftRightMovement();
+        Jumping();
+    }
+
+    void LeftRightMovement()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(move * maxSpeed,
+            rigidbody2D.velocity.y);
+    }
+    void Jumping()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
 }
