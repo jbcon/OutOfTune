89735f925afac091bc1dcaa5ea9fff3e20fe7ea1
John Conover <111jbcon@gmail.com>
2015-02-21 20:14:19 -0500

    Added firing projectiles, but they won't show up

---
+++ OutOfTune/Assets/Scripts/Player/WeaponProperties.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponProperties : MonoBehaviour {
+
+    public GameObject projectile;
+    public float weaponForce;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void Fire(Vector2 direction)
+    {
+        GameObject b = Instantiate(projectile) as GameObject;
+        b.transform.SetParent(transform);
+        b.rigidbody2D.AddForce(direction*weaponForce, ForceMode2D.Impulse);
+        b.SetActive(true);
+    }
+}
