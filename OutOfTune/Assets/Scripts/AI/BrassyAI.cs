8d30a819af097db8e879e1c9a2dbbdd214b9ff29
hizzy <leeh14@rpi.edu>
2015-03-25 23:14:56 -0400

    Brassy AI proto

---
+++ OutOfTune/Assets/Scripts/AI/BrassyAI.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrassyAI : MonoBehaviour {
+	//walking forward and shooting at different 
+	public GameObject player;
+	public int speed = 20;
+	public Transform player_loc;
+	public int health = 10;
+	public float jumping = 200f;
+	
+	//how far until it can't see the player
+	public float range = 50.0f;
+
+	bool jump;
+	bool faceright;
+	float pos_scale;
+	// Use this for initialization
+	void Start () {
+		pos_scale = transform.localScale.x;
+		faceright = true;
+		jump = true;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		player_loc = player.transform;
+	}
+	public void Defend(int dmg){
+		health -= dmg;
+		if (health <= 0)
+		{
+			gameObject.GetComponent<CircleCollider2D>().enabled = false;
+			gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+			StartCoroutine("Die");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance(player.transform.position, gameObject.transform.position);
+		
+		//move only if not hurt
+		if (distance < range && health > 0)
+		{
+			//animator.SetBool("Walking", true);
+			//animator.SetBool("Idle", false);
+			Movement();
+		}
+		else
+		{
+			Debug.Log("doing nothing");
+			//animator.SetBool("Walking", false);
+			//animator.SetBool("Idle", true);
+		}
+	}
+	void Leap(){
+		if (jump == true) {
+			gameObject.GetComponent<Rigidbody2D>().AddForce (Vector3.up * jumping);
+			//rigidbody2D.AddForce (Vector3.up * jumping);
+		}
+	}
+	void Movement(){
+		// using the point to determine if the ai is on the left or right side of the player
+		
+		Vector3 point = player_loc.InverseTransformPoint (transform.position);
+		if (point.x > 0) {
+			if (player.GetComponent<PlayerController>().grounded)
+				faceright = true;
+		} else if (point.x < 0) {
+			if(player.GetComponent<PlayerController>().grounded)
+				faceright = false;
+		}
+		
+		if (faceright == true)
+		{
+			//rotate the sprite to face the correct direction if hes on the left
+			transform.localScale = new Vector2(-pos_scale, transform.localScale.y);
+			//left side of the player move left
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+		else
+		{
+			transform.localScale = new Vector2(pos_scale, transform.localScale.y);
+			//right side of the player move right
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+	}
+	private IEnumerator Die()
+	{
+		//animator.SetTrigger("Hurt");
+		yield return new WaitForSeconds(1.0f);
+		Destroy(gameObject);
+	}
+}
