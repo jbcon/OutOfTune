5ae69cc90ef1cc3a707d8550a47c96d4b168b41e
hizzy <leeh14@rpi.edu>
2015-04-02 18:56:47 -0400

    level transitions in place

---
+++ OutOfTune/Assets/Transitionlevel3.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transitionlevel3 : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		int playerLayer = LayerMask.NameToLayer("Player");
+		
+		if (collision.gameObject.layer == playerLayer)
+		{
+			//Debug.Log("HIT!");
+			Application.LoadLevel("Level 3");
+		}
+	}
+}
