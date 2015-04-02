5cd04e5aa9c34eddae22970065e2b5f78579e837
Phrawger <phrawger@gmail.com>
2015-04-02 13:53:42 -0400

    Level 2 Geometry complete
    I've finished the actual "level" bits of Level 2. Now featuring moving
    platforms, slightly sticky at the top of their arcs but otherwise not
    really glitchy, which I'm content with for now.

---
+++ OutOfTune/Assets/MovingPlatform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -298 || transform.position.y > -222) {
+			speed *= -1;
+		}
+		transform.Translate(Vector3.up * speed);
+	}
+}
