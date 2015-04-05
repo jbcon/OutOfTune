09d5f76264b04c1beb53ed546583865cc506f1a2
hizzy <leeh14@rpi.edu>
2015-04-04 21:51:41 -0400

    save and load functionality there not quite 100%

--- OutOfTune/Assets/Scripts/Player/SaveLoad.cs
+++ OutOfTune/Assets/Scripts/Player/SaveLoad.cs
@@ -1,38 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
-public class SaveLoad : MonoBehaviour {
-	public string[] fileNames = new string[]{"player1","player2"} ;
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+public class SaveLoad: MonoBehaviour{
+	public float health = 10;
+	public int level = 1;
+	public Vector3 checkpointlocation = new Vector3(0,0,0);
+
 	public void Load(){
-		PlayerData data = new PlayerData();
-		FileStream file = File.Open(Application.persistentDataPath + fileNames[0]+"info.dat", FileMode.Open); 
-		BinaryFormatter bf = new BinaryFormatter();
-		//bf.Binder = new VersionDeserializationBinder();
-		data = (PlayerData)bf.Deserialize(file);
-		file.Close();
+		if(File.Exists("playervariables.gd")) {
+			PlayerData playervalues = new PlayerData();
+			Stream stream = File.Open("playervariables.gd", FileMode.Open);
+			BinaryFormatter bformatter = new BinaryFormatter();          
+			UnityEngine.Debug.Log("Loading variables");
+			playervalues = (PlayerData)bformatter.Deserialize(stream);
+			Debug.Log(" level " +playervalues.level);
+			Debug.Log("healhe " + playervalues.health);
+			stream.Close();
+		}
 	}
 	public void Save(){
-		BinaryFormatter bf = new BinaryFormatter();
-		FileStream file = File.Open(Application.persistentDataPath + fileNames[0]+"info.dat" , FileMode.Open);
-		PlayerData data = new PlayerData();
-		data.health = 10;
-		bf.Serialize(file,data);
-		file.Close();
+		PlayerData playervalues = new PlayerData();
+		playervalues.health = health;
+		playervalues.level = level;
+		//playervalues.checkpointlocation = checkpointlocation;
+
+		Stream stream = File.Open("playervariables.gd", FileMode.Create);
+		BinaryFormatter bformatter = new BinaryFormatter();            
+		Debug.Log("Saving variables");
+		bformatter.Serialize(stream, playervalues);
+		stream.Close();
+	}
+	public void increase(){
+		health +=10;
+	}
+	public void printinfo(){
+		Debug.Log (health);
 	}
 }
-[Serializable]
-class PlayerData{
+[System.Serializable]
+public class PlayerData{
+	public static PlayerData current;
 	public float health;
-	public float level;
+	public int level;
 	public float checkpoint;
+	//public Vector3 checkpointlocation;
+	public PlayerData(){
+		this.health = 10;
+		this.level = 1;
+		this.checkpoint = 1;
+		//this.checkpointlocation = new Vector3(0,0,0);
+	}
+	// deserialization constructor
+	public PlayerData(SerializationInfo info, StreamingContext ctxt ){
+		health = (float)info.GetValue("health", typeof(float));
+		level = (int)info.GetValue("level", typeof(int));
+		//checkpointlocation = (Vector3)info.GetValue("checkpointlocation", typeof(Vector3));
+	}
+	//serialization function
+	public void GetObjectData(SerializationInfo info, StreamingContext ctxt){
+		info.AddValue("health", health);
+		info.AddValue("level", level);
+		//info.AddValue("checkpointlocation", checkpointlocation);
+	}
 }
---
+++ OutOfTune/Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+	public int testcounter =0;
+	public void StartGame(){
+		Application.LoadLevel("Level 1");
+	}
+	public void increment(){
+		testcounter++;
+	}
+	public void getcounter(){
+		Debug.Log(testcounter);
+	}
+}
