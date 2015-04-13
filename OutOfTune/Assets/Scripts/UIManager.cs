c1a9f57c51f285c24d26f777b0554f689996b080
hizzy <leeh14@rpi.edu>
2015-04-12 21:37:28 -0400

    adding setup for button selection menu

--- OutOfTune/Assets/Scripts/UIManager.cs
+++ OutOfTune/Assets/Scripts/UIManager.cs
@@ -5,11 +5,20 @@ public class UIManager : MonoBehaviour {
 	public Animator settings;
 	public Animator Save;
 	public bool clicked = false;
+	private List<string> buttonnames = new List<string>();
 	public void Awake(){
 		DontDestroyOnLoad(gameObject);
 	}
+	public void Start(){
+	}
 	public void StartGame(){
 		Application.LoadLevel("Level 1");
+	}
+	public void LoadLevel2(){
+		Application.LoadLevel("Level 2");
+	}
+	public void LoadLevel3(){
+		Application.LoadLevel("Level 3");
 	}
 	public void Update(){
 		if (Input.GetKeyDown((KeyCode.Escape)))
