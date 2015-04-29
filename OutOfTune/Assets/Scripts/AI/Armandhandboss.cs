bf2e2fecb9ee8d96fd3e4bd87766fdb7a309812b
John Conover <111jbcon@gmail.com>
2015-04-29 05:40:42 -0400

    Added rudimentary but unfinished Armand boss.

--- OutOfTune/Assets/Scripts/AI/Armandhandboss.cs
+++ OutOfTune/Assets/Scripts/AI/Armandhandboss.cs
@@ -1,34 +1,144 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+public enum BossState
+{
+    Resting,
+    Slapping,
+    Drumming,
+    Clapping,
+    BigAimSlapping
+}
+
 public class Armandhandboss : MonoBehaviour {
 	public GameObject player;
-	private bool fight;				//initialize the fight
-    private bool attack;
+
+    public Sprite palm;
+    public Sprite fist;
+
+    public bool bossActive;
+
+    private GameObject leftHand;
+    private GameObject rightHand;
+    private Animator animator;
+
+    private BossState bossState;
 	private float speed;
 	private float counter;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
-		fight = false;
+        leftHand = GameObject.FindGameObjectWithTag("LeftHand");
+        rightHand = GameObject.FindGameObjectWithTag("RightHand");
+        animator = GetComponent<Animator>();
 		speed = 2;
 		counter = 0;
+        bossState = BossState.Resting;
+        bossActive = false;
 	}
-	
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            GameObject camera = GameObject.FindGameObjectWithTag("CameraManager");
+            CameraManager cManager = camera.GetComponent<CameraManager>();
+            bossActive = true;
+        }
+    }
+
+
 	// Update is called once per frame
 	void Update () {
-		float distance = Vector3.Distance(player.transform.position, gameObject.transform.position);
-		if (distance < 10){
-			fight = true;
-		}
-		if (fight == true){
-			int method = Random.Range(0,3);		//randomly choose one of three ai behaviors
-			//StartCoroutine("Slap");
-		}else{
-			Debug.Log ("stop the coroutines");
-		}
+        if (bossActive)
+        {
+            Debug.Log(bossState);
+            switch (bossState)
+            {
+                //no action
+                case BossState.Resting:
+                    //choose a new action
+                    StartCoroutine("Resting");
+                    break;
+
+                //rhythmic drumming on stage
+                case BossState.Drumming:
+                    StartCoroutine("Drumming");
+                    break;
+
+                //idk
+                case BossState.Slapping:
+                    StartCoroutine("Slapping");
+                    break;
+
+                //aims for player, then slams the ground
+                case BossState.BigAimSlapping:
+                    StartCoroutine("BigAimSlapping");
+                    break;
+
+                //clapping his hands
+                case BossState.Clapping:
+                    StartCoroutine("Clapping");
+                    break;
+            }
+        }
 	}
-	void Slap()
+
+    IEnumerator Resting()
+    {
+        //randomly choose new state every second or few
+        yield return new WaitForSeconds(Random.Range(5, 10));
+        int attack = Random.Range(0, 4);
+        switch (attack)
+        {
+            case 0:
+                bossState = BossState.Drumming;
+                break;
+
+            case 1:
+                bossState = BossState.Slapping;
+                break;
+
+            case 2:
+                bossState = BossState.BigAimSlapping;
+                break;
+
+            case 3:
+                bossState = BossState.Clapping;
+                break;
+        }
+    }
+
+    IEnumerator Drumming()
+    {
+        //just does the drumming animation
+        animator.SetBool("Drumming", true);
+        yield return new WaitForSeconds(3.0f);
+        bossState = BossState.Resting;
+        animator.SetBool("Drumming", false);
+    }
+
+    IEnumerator Slapping()
+    {
+        yield return new WaitForSeconds(5.0f);
+        bossState = BossState.Resting;
+    }
+
+    IEnumerator BigAimSlapping()
+    {
+        yield return new WaitForSeconds(5.0f);
+        bossState = BossState.Resting;
+    }
+
+    IEnumerator Clapping()
+    {
+        yield return new WaitForSeconds(5.0f);
+        bossState = BossState.Resting;
+    }
+
+    
+	/*void Slap()
 	{
 		while(counter < 10){
 			//going to follow player on top of him then wait then smash
@@ -41,10 +151,8 @@ public class Armandhandboss : MonoBehaviour {
 				gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
 			}else {
 				//righht on top of it
-				attack = true;
 			}
-			fight = false;
 		}
 
-	}
+	}*/
 }
