ea74365e28635ce2be172bda7810ec11af7e576b
hizzy <leeh14@rpi.edu>
2015-04-30 18:24:26 -0400

    stroy implementatin there just missing the activation of the story

--- OutOfTune/Assets/Scripts/Story.cs
+++ OutOfTune/Assets/Scripts/Story.cs
@@ -5,14 +5,29 @@ using System.Collections.Generic;
 using System.IO;
 public class Story : MonoBehaviour {
 	private Text story;
+	private Image background;
 	private List<string> story_book = new List<string>();
+	//private List<string> storylines = new List<><string>();
 	private GameObject conversation;
+	public bool display;
+	private int storyiterator;
+	private int sentence_iterator;
 	// Use this for initialization
 
 	void Start () {
-		conversation = GameObject.Find("storysetting");
-		story = GetComponent<Text>();
-
+		display = false;
+		storyiterator = 1;
+		sentence_iterator = 0;
+		conversation = GameObject.FindGameObjectWithTag("Story");
+		story = conversation.GetComponentInChildren<Text>();
+		background = conversation.GetComponentInChildren<Image>();
+		/*
+		if (background == null){
+			Debug.Log("failed");
+		}else{
+			Debug.Log("sucess");
+		}
+		*/
 		var sr = new StreamReader(Application.dataPath +"/scripts/Dialogue.txt");
 		var contents = sr.ReadToEnd();
 		var lines = contents.Split("\n"[0]);
@@ -20,9 +35,37 @@ public class Story : MonoBehaviour {
 		for ( temp = 0 ; temp < lines.Length ; temp ++){
 			story_book.Add(lines[temp]);
 		}
-		story.text = story_book[0];
+		//story.text = story_book[0];
 		//conversation.GetComponent<GUIText>().enabled = false;
-		story.enabled = false;
-	}
+		//story.enabled = false;
 
+		story.enabled = false;
+		background.enabled = false;
+
+
+	}
+	void Update(){
+		if (display == true){
+			background.enabled = true;
+			var storylines = story_book[storyiterator].Split(" "[0]);
+			if(storylines[0] == "Armand:" ||  storylines[0] == "Narrator:" ||
+			   storylines[0] == "Renee:" || storylines[0] == "Peter:" ||
+			   storylines[0] == "Maestro:"
+			   )
+			{
+				story.text = story_book[storyiterator];
+			}else{
+				storyiterator++;
+				//be at end so
+				display = false;
+			}
+			story.enabled = true;
+			if (Input.GetKeyDown((KeyCode.Return))){
+				storyiterator ++;
+			}
+		}else{
+			story.enabled = false;
+			background.enabled = false;
+		}
+	}
 }
